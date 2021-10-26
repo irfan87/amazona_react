14246c7668235017644d1f5679b75257ff3d108f
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-10-26 17:44:37 +0800

    load products from server

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
-import data from "../data";
+
+import axios from "axios";
+import Loading from "../components/Loading";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-	const { products } = data;
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		const fetchProductsData = async () => {
+			try {
+				setLoading(true);
+
+				const { data } = await axios.get("/api/products");
+
+				setLoading(false);
+
+				setProducts(data);
+			} catch (err) {
+				setError(err.message);
+				setLoading(false);
+			}
+		};
+
+		fetchProductsData();
+	}, []);
 
 	return (
-		<div className="row center">
-			{products.map((product, index) => (
-				<Product product={product} key={index} />
-			))}
-		</div>
+		<>
+			{loading ? (
+				<Loading />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<div className="row center">
+					{products.map((product, index) => (
+						<Product product={product} key={index} />
+					))}
+				</div>
+			)}
+		</>
 	);
 };
 
