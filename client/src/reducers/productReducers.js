72312288316368b2e7cd4d6bcdbeecc498e7bc59
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-10-27 11:23:28 +0800

    apply redux in product details screen

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+	PRODUCT_DETAILS_FAILED,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_LIST_FAILED,
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
@@ -14,6 +17,22 @@ export const productListReducer = (
 		case PRODUCT_LIST_SUCCESS:
 			return { loading: false, products: action.payload };
 		case PRODUCT_LIST_FAILED:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+
+export const productDetailsReducer = (
+	state = { loading: true, product: {} },
+	action
+) => {
+	switch (action.type) {
+		case PRODUCT_DETAILS_REQUEST:
+			return { loading: true };
+		case PRODUCT_DETAILS_SUCCESS:
+			return { loading: false, product: action.payload };
+		case PRODUCT_DETAILS_FAILED:
 			return { loading: false, error: action.payload };
 		default:
 			return state;
