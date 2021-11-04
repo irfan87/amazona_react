80d2c849867da1795bb43845e4d60e52688e7a19
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-11-05 00:08:16 +0800

    implement signin action

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -1,20 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const SigninScreen = () => {
+import { signin } from "../actions/userActions";
+import Loading from "../components/Loading";
+import Message from "../components/Message";
+
+const SigninScreen = (props) => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+
+	const redirect = props.location.search
+		? props.location.search.split("=")[1]
+		: "/";
+
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo, loading, error } = userSignin;
+
+	const dispatch = useDispatch();
 
 	const submitHandler = (e) => {
 		e.preventDefault();
 
-		// TODO: sign in action
+		dispatch(signin(email, password));
 	};
+
+	useEffect(() => {
+		if (userInfo) {
+			props.history.push(redirect);
+		}
+	}, [props.history, redirect, userInfo]);
+
 	return (
 		<div>
 			<form className="form" onSubmit={submitHandler}>
 				<div>
 					<h1>Sign In</h1>
+					{loading && <Loading />}
+					{error && <Message variant="danger">{error}</Message>}
 				</div>
 				<div>
 					<label htmlFor="email">Email Address</label>
